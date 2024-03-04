43c236d56e111da14f2602315784013a420ecb0c
Davi Pedro <davisilvaps02@gmail.com>
2024-03-04 14:59:39 -0300

    feat(DTO): creating listarContas, buscarPorNumero, preencherContaDTO methods and some improvements

--- src/main/java/br/com/alura/bytebank/DAO/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/DAO/ContaDAO.java
@@ -1,31 +1,31 @@
 package br.com.alura.bytebank.DAO;
 
+import br.com.alura.bytebank.DTOs.ContasDTO;
+import br.com.alura.bytebank.domain.RegraDeNegocioException;
 import br.com.alura.bytebank.domain.cliente.Cliente;
 import br.com.alura.bytebank.domain.conta.Conta;
-import br.com.alura.bytebank.domain.conta.DadosAberturaConta;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContaDAO {
 
-    private Connection connection;
+    private final Connection connection;
 
     public ContaDAO(Connection connection) {
         this.connection = connection;
-    }
-
-    private PreparedStatement createPreparedStatement(String sql) throws SQLException {
-        return connection.prepareStatement(sql);
     }
 
     public void salvar(Cliente cliente, Conta conta){
         String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
                 "VALUES (?,?,?,?,?)";
 
-        try(var preparedStatement = createPreparedStatement(sql)){
+        try(var preparedStatement = connection.prepareStatement(sql)){
             preparedStatement.setInt(1, conta.getNumero());
             preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
             preparedStatement.setString(3, cliente.getNome());
@@ -36,6 +36,57 @@ public class ContaDAO {
         }
         catch (SQLException e){
             throw new RuntimeException(e);
+        }
+    }
+
+    public String listarContas(){
+        String sql = "SELECT * FROM conta";
+
+        Set<ContasDTO> contas = new HashSet<>();
+
+        try (var preparedStatement = connection.prepareStatement(sql)){
+            preencherContaDTO(contas, preparedStatement);
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return contas.toString();
+    }
+
+    public Conta buscarPorNumero(Integer num){
+        String sql = "SELECT * FROM conta WHERE numero = ?";
+
+        Set<ContasDTO> contas = new HashSet<>();
+
+        try (var preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.setInt(1, num);
+
+            preencherContaDTO(contas, preparedStatement);
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return contas.stream().findFirst().map(contaDTO -> new Conta(
+                contaDTO.getNumero(),
+                new Cliente(
+                        contaDTO.getCliente_nome(),
+                        contaDTO.getCliente_cpf(),
+                        contaDTO.getCliente_email()
+                )
+        )).orElseThrow(() -> new RegraDeNegocioException("A conta não foi encontrada"));
+    }
+
+    private void preencherContaDTO(Set<ContasDTO> contas, PreparedStatement preparedStatement) throws SQLException {
+        try (ResultSet resultSet  = preparedStatement.executeQuery()){
+            while (resultSet.next()){
+                Integer numero = resultSet.getInt(1);
+                BigDecimal saldo = resultSet.getBigDecimal(2);
+                String nome = resultSet.getString(3);
+                String cpf = resultSet.getString(4);
+                String email = resultSet.getString(5);
+
+                contas.add(new ContasDTO(numero, saldo, nome, cpf, email));
+            }
         }
     }
 }
