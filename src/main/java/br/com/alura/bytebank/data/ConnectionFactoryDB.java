ecbdba74992db986094afce5fcb4ec62174c494d
Davi Pedro <davisilvaps02@gmail.com>
2024-03-04 08:51:15 -0300

    refactor(DB connection): removing singleton pattern on DB connection

--- src/main/java/br/com/alura/bytebank/data/ConnectionFactoryDB.java
+++ src/main/java/br/com/alura/bytebank/data/ConnectionFactoryDB.java
@@ -4,30 +4,25 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionDB {
+public class ConnectionFactoryDB {
 
     private static final String USER = "postgres";
     private static final String PASSWORD = "D02dps$";
     private static final String URL_CONNECTION = "jdbc:postgresql://localhost/byte_bank";
 
-    private static Connection connection;
+    private Connection connection;
 
-    private ConnectionDB() {
-    }
-
-    public static Connection getConnection(){
-        if (connection == null){
-            try{
-                connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
-            }
-            catch (SQLException e){
-                throw new RuntimeException(e);
-            }
+    public Connection getConnection(){
+        try{
+            connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
         }
         return connection;
     }
 
-    public static void closeConnection(){
+    public void closeConnection(){
         if (connection != null){
             try {
                 connection.close();
