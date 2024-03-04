e2503fb69704fc2c4b7e54ed9914c1152f436798
Davi Pedro <davisilvaps02@gmail.com>
2024-03-04 08:49:33 -0300

    feat(DB): adding JDBC query to abrir method

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -1,19 +1,31 @@
 package br.com.alura.bytebank.domain.conta;
 
+import br.com.alura.bytebank.data.ConnectionFactoryDB;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
 import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
 
+    private Connection connection;
+
     private Set<Conta> contas = new HashSet<>();
+
+    public ContaService() {
+        var connectionFactory = new ConnectionFactoryDB();
+        connection = connectionFactory.getConnection();
+    }
 
     public Set<Conta> listarContasAbertas() {
         return contas;
     }
+
 
     public BigDecimal consultarSaldo(Integer numeroDaConta) {
         var conta = buscarContaPorNumero(numeroDaConta);
@@ -27,7 +39,25 @@ public class ContaService {
             throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
         }
 
-        contas.add(conta);
+        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf, cliente_email)" +
+                "VALUES (?,?,?,?,?)";
+
+        try(var preparedStatement = createPreparedStatement(sql)){
+            preparedStatement.setInt(1, dadosDaConta.numero());
+            preparedStatement.setBigDecimal(2,BigDecimal.ZERO);
+            preparedStatement.setString(3, cliente.getNome());
+            preparedStatement.setString(4,cliente.getCpf());
+            preparedStatement.setString(5, cliente.getEmail());
+
+            preparedStatement.execute();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement createPreparedStatement(String sql) throws SQLException {
+        return connection.prepareStatement(sql);
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
