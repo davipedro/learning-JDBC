7bca8e210ee365d02b98b7f78c0ec8cbcfab6a8f
Davi Pedro <davisilvaps02@gmail.com>
2024-03-04 20:10:47 -0300

    refactor(Pooling): changing the connection to HikariDataSource

--- src/main/java/br/com/alura/bytebank/data/ConnectionFactoryDB.java
+++ src/main/java/br/com/alura/bytebank/data/ConnectionFactoryDB.java
@@ -1,34 +1,32 @@
 package br.com.alura.bytebank.data;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactoryDB {
-
     private static final String USER = "postgres";
-    private static final String PASSWORD = "D02dps$";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
     private static final String URL_CONNECTION = "jdbc:postgresql://localhost/byte_bank";
 
-    private Connection connection;
-
-    public Connection getConnection(){
+    public static Connection getConnection(){
         try{
-            connection = DriverManager.getConnection(URL_CONNECTION, USER, PASSWORD);
+            return createDataSource().getConnection();
         }
         catch (SQLException e){
             throw new RuntimeException(e);
         }
-        return connection;
     }
 
-    public void closeConnection(){
-        if (connection != null){
-            try {
-                connection.close();
-            } catch (SQLException e){
-                throw new RuntimeException(e);
-            }
-        }
+    private static HikariDataSource createDataSource(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(URL_CONNECTION);
+        config.setUsername(USER);
+        config.setPassword(PASSWORD);
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
